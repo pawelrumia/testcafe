55c4d10221c4069286dc7fa43396c3813e3c419e
Pawel.Mazur <pawelomazzzz@gmail.com>
2023-08-09 16:12:04 +0200

    Initial commit testcafe2

---
+++ tests/testDropdown.js
@@ -0,0 +1,8 @@
+import { Selector } from 'testcafe';
+
+const interfaceDropdown = Selector("#preferred-interface")
+
+fixture("Dropdown fixture")
+    .page("https://devexpress.github.io/testcafe/example/");
+
+    
